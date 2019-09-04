bc881959221e121dcaa00691b863c579c9d48c42
bandaloo <bandaloo@icloud.com>
2019-09-04 16:35:06 +0900

    changed to ino file

--- ritsu-heart.ino
+++ ritsu-heart.ino
@@ -12,7 +12,7 @@
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 // Analog pin for reading flow rate potentiometer value
@@ -46,11 +46,11 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
-
 /**
  * Runs once at startup.
  */
-void setup() {
+void setup()
+{
   Serial.begin(9600); // Need this to write to the console (Serial monitor)
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
@@ -66,11 +66,12 @@ void setup() {
 /**
  * Runs repeatedly as fast as the board can execute it, like a while(1) loop.
  */
-void loop() {
+void loop()
+{
   // Read pot voltage and map to an appropriate speed for the blood flow rate
   float pinValue = analogRead(FLOW_RATE_POT_PIN);
   float bloodSpeed = mapf(pinValue, 0, ANALOG_RANGE, MIN_SPD, MAX_SPD);
-  
+
   // Read pot voltage and map to an appropriate pressure for the blood coloration
   float pinValue = analogRead(PRESSURE_POT_PIN);
   float bloodSpeed = mapf(pinValue, 0, ANALOG_RANGE, MIN_PRESSURE, MAX_PRESSURE);
@@ -101,22 +102,24 @@ float timeLED = 0;
  * Creates a "blood flow" light design with on the given strip with the given pressure (kPa)
  * and speed (independent of LED_DELAY). Call in a loop to animate.
  */
-void bloodFlowLED(float pressure, float spd) {
+void bloodFlowLED(float pressure, float spd)
+{
   // Frequency of light wave effect
   float sinFreq = 0.07;
 
   // Update time
   timeLED += spd * LED_DELAY;
-  
-  for (int i = 0; i < LED_COUNT; i++) {
+
+  for (int i = 0; i < LED_COUNT; i++)
+  {
     float intensity = 0.5 * sin(sinFreq * (2 * PI) * i + t) + 0.5;
     uint32_t color = strip.Color(
-      (int)(intensity * r),
-      (int)(intensity * g),
-      (int)(intensity * b));
+        (int)(intensity * r),
+        (int)(intensity * g),
+        (int)(intensity * b));
     strip.setPixelColor(i, color);
   }
-  
+
   // Write the set values to the real-world LEDs
   strip.show();
 }
@@ -125,15 +128,16 @@ void bloodFlowLED(float pressure, float spd) {
  * Maps a number in the specified "from" range to the "to" range (e.g. mapping 0.5 from the range [0, 1]
  * to the range [0, 10] returns 5.0). 
  */
-float mapf(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
+float mapf(float value, float fromLow, float fromHigh, float toLow, float toHigh)
+{
   // Map to 0.0 to 1.0 (inclusive)
   float rangeLow = fromHigh - fromLow;
   value = (value - fromLow) / rangeLow;
-  
+
   // Map to toLow to toHigh (inclusive)
   float rangeHigh = toHigh - toLow;
   value = value * rangeHigh + toLow;
-  
+
   return value;
 }
 
@@ -144,23 +148,29 @@ float mapf(float value, float fromLow, float fromHigh, float toLow, float toHigh
 // (as a single 'packed' 32-bit value, which you can get by calling
 // strip.Color(red, green, blue) as shown in the loop() function above),
 // and a delay time (in milliseconds) between pixels.
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
+void colorWipe(uint32_t color, int wait)
+{
+  for (int i = 0; i < strip.numPixels(); i++)
+  {                                // For each pixel in strip...
+    strip.setPixelColor(i, color); //  Set pixel's color (in RAM)
+    strip.show();                  //  Update strip to match
+    delay(wait);                   //  Pause for a moment
   }
 }
 
 // Theater-marquee-style chasing lights. Pass in a color (32-bit value,
 // a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
 // between frames.
-void theaterChase(uint32_t color, int wait) {
-  for(int a=0; a<10; a++) {  // Repeat 10 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
+void theaterChase(uint32_t color, int wait)
+{
+  for (int a = 0; a < 10; a++)
+  { // Repeat 10 times...
+    for (int b = 0; b < 3; b++)
+    {                //  'b' counts from 0 to 2...
+      strip.clear(); //   Set all pixels in RAM to 0 (off)
       // 'c' counts up from 'b' to end of strip in steps of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
+      for (int c = b; c < strip.numPixels(); c += 3)
+      {
         strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
       }
       strip.show(); // Update strip with new contents
@@ -170,13 +180,16 @@ void theaterChase(uint32_t color, int wait) {
 }
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
+void rainbow(int wait)
+{
   // Hue of first pixel runs 5 complete loops through the color wheel.
   // Color wheel has a range of 65536 but it's OK if we roll over, so
   // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
   // means we'll make 5*65536/256 = 1280 passes through this outer loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+  for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256)
+  {
+    for (int i = 0; i < strip.numPixels(); i++)
+    { // For each pixel in strip...
       // Offset pixel hue by an amount to make one full revolution of the
       // color wheel (range of 65536) along the length of the strip
       // (strip.numPixels() steps):
@@ -194,19 +207,23 @@ void rainbow(int wait) {
 }
 
 // Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int a=0; a<30; a++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
+void theaterChaseRainbow(int wait)
+{
+  int firstPixelHue = 0; // First pixel starts at red (hue 0)
+  for (int a = 0; a < 30; a++)
+  { // Repeat 30 times...
+    for (int b = 0; b < 3; b++)
+    {                //  'b' counts from 0 to 2...
+      strip.clear(); //   Set all pixels in RAM to 0 (off)
       // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
+      for (int c = b; c < strip.numPixels(); c += 3)
+      {
         // hue of pixel 'c' is offset by an amount to make one full
         // revolution of the color wheel (range 65536) along the length
         // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        int hue = firstPixelHue + c * 65536L / strip.numPixels();
         uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+        strip.setPixelColor(c, color);                       // Set pixel 'c' to value 'color'
       }
       strip.show();                // Update strip with new contents
       delay(wait);                 // Pause for a moment
