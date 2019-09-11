50347abc62896b50e4a1efe18a741de71c0e9249
bandaloo <bandaloo@icloud.com>
2019-09-11 15:40:50 +0900

    got the model working in the main loop

--- ritsu-heart.ino
+++ ritsu-heart.ino
@@ -48,10 +48,38 @@ float prevTime = 0;
 float deltaTime;
 float timeLED = 0;
 
+// things for the model
+double Vlv = 100.0;
+double Pa = 80.0;
+
+double Pv = 10.0; // we can change this
+double Po = 10.0;
+double Rv = 0.01;
+double R0 = 0.005;
+double R = 1.0; // we can change this
+double C = 5.0; // we can change this
+
+double maxtime = 1.0;
+double dt = 0.001;
+double trr = 1.0;
+double ts;
+double es = 1.0;
+double ed = 0.1;
+double e;
+
+int i;
+int maxstep;
+double t;
+
+// end of things for the model
+
 /**
  * Runs once at startup.
  */
 void setup() {
+    ts = 0.3 * sqrt(trr);
+    maxstep = maxtime / dt;
+
     setupDigit(20); // run setup for the first motor
     Serial.begin(9600); // Need this to write to the console (Serial monitor)
 
@@ -60,10 +88,55 @@ void setup() {
     strip.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
+int simSteps = 0;
+
 /**
  * Runs repeatedly as fast as the board can execute it, like a while(1) loop.
  */
 void loop() {
+    double fv;
+    double f1;
+    double f2;
+
+    // E(t)の計算 (calculation)
+    for (int i = 0; i < LED_DELAY; i++) {
+        t = fmod(simSteps * dt, trr);
+        simSteps++;
+        if (t < ts) {
+            e = ed + (es - ed)/2.0 * (1.0 - cos(M_PI * t / (0.3 * sqrt(trr))));
+        } else if (t < (ts * 3.0 / 2.0)) {
+            e = ed + (es - ed)/2.0 * (1.0 + cos(2.0 * M_PI * (t - 0.3 * sqrt(trr)) / (0.3 * sqrt(trr))));
+        } else {
+            e = ed;
+        }
+
+        double Plv = e * Vlv;
+        
+        // fv, f1, f2
+        fv = (Pv - Plv) / Rv;
+        f1 = (Plv - Pa) / R0;
+        f2 = (Pa - Po) / R;
+        
+        // Vlv, Pa
+        Vlv += (fv - f1) * dt;
+        Pa += (f1 - f2) * e * dt;
+    }
+
+    //printf("%lf %lf %lf\n", t, Vlv, Pa);
+    Serial.print(t);
+    Serial.print(" ");
+    Serial.print(Vlv);
+    Serial.print(" ");
+    Serial.print(Pa);
+    Serial.print(" ");
+    Serial.print(fv);
+    Serial.print(" ");
+    Serial.print(f1);
+    Serial.print(" ");
+    Serial.print(f2);
+    Serial.print("\n");
+
+    // arduino stuff (not related to model)
     float currentTime = millis();
     deltaTime = currentTime - prevTime;
     prevTime = currentTime;
